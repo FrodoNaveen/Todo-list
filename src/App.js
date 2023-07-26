56b1eed4fafa22511e11fa64cbf9b92e7e469716
FrodoNaveen <frodonaveen33@gmail.com>
2023-07-26 17:30:32 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+/* eslint-disable no-redeclare */
+
 import './App.css';
+import {useState} from "react";
+import { v4 as uuidv4 } from "uuid";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [data,setData] = useState("")
+  const [createData,setCreateData] = useState([])
+   
+  function newData(event) {
+    event.preventDefault()
+    setData(event.target.value);
+   }
+  
+  function add(e){
+  
+   setCreateData((prevState) => {
+    // console.log("uuidv4--- ", uuidv4());
+    return [{data,id:uuidv4(),isChecked:false}, ...prevState]
+    });
+    setData("")
+  }
+
+  let date=new Date()
+  let text = date.toLocaleDateString()
+
+
+  function changeData(id){
+    console.log(id)
+    let newData = createData.map((ele)=>{
+      if (id===ele.id){
+        return{...ele,
+               isChecked:!ele.isChecked}
+      }else{
+        return ele
+      }
+   })
+   
+    setCreateData(newData)
+  }
+
+  function deleteTasks(id){
+    console.log(id)
+    const newData = createData.filter((elem) => elem.id !== id)
+    console.log("newData--- ", newData);
+  // let index = createData.map((ele)=>{
+  //   return ele.id
+  // }).indexOf(id)
+  // let c = createData.splice(index,1)
+  // let b = createData.map((ele)=>{
+  //   if(id===ele.id){
+  //     return c
+  //   }else{
+  //     return ele
+  //   }
+
+  // })
+  setCreateData(newData);
+  }
+ 
+  
+ return (
+   <div>
+     <div className="nav1">
+       <p className="nav1p">Log in</p>
+       <p className="navlist">+NewList</p>
+     </div>
+     <nav className="nav2">
+       <h3 className="navhead">My to-do list {text}</h3>
+       <button  className="addbtn" onClick={add}>
+         Save This List
+       </button>
+     </nav>
+     <input
+       className="input" 
+       type="text"
+       onChange={newData}
+       placeholder="Write Your Next Tasks Here...."
+       value={data} 
+       
+     ></input>
+     <br></br>
+     <p className="tasks">
+      
+       {createData.map((ele) =>{
+       return (
+         <li
+          style={{ textDecoration: ele.isChecked ? "line-through" : "none" }}
+           className="list"
+           key={ele.id}
+           >
+           <input onChange={()=>changeData(ele.id)} type="checkbox"></input> {ele.data}
+           <button onClick={()=>deleteTasks(ele.id)}className="delbtn" type='button'>
+            Delete
+           </button>
+           
+         </li>
+       )})}
+     </p>
+   </div>
+ );
+}
 export default App;
